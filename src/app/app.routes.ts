329e7115517b64ecc873766c898f35511b247a7e
Fernando Zabala <ortegafernando334@gmail.com>
2025-10-21 15:42:21 -0500

    crear componente header y footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', component: Home },
+    // { path: 'search', component: SearchPage },
+    // { path: 'movies', component: MoviesPage },
+    // { path: 'series', component: SeriesPage },
+    { path: '**', redirectTo: '' } // redirige si no encuentra la ruta
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './shared/components/header/header';
+import { Footer } from './shared/components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Header,
+    Footer
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
